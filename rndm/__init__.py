a437aba1cb98be2d54f3b24bb455dbad2a76b496
kh-ops69 <krishhashia19@gmail.com>
2025-01-06 13:32:29 +0530

    removed unnecessary python files

--- rndm/__init__.py
+++
@@ -1,8 +0,0 @@
-# Registry of functionalities and descriptions
-package_help = {
-    "help": "Show all available modules/functions and their descriptions.",
-    "list": "List items.",
-    "show": "Show details of an item.",
-    "add": "Add a new item.",
-    "greet": "Check AI tracking availability.",
-}
